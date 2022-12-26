34b1e9c6bfbd03e12d4e970fc400c5be32445764
sschr15 <steven.sschr15@hotmail.com>
2022-12-26 08:13:04 -0600

    Alphabetize tests

--- plugins/kotlin/src/test/java/org/quiltmc/quiltflower/kotlin/KotlinTests.java
+++ plugins/kotlin/src/test/java/org/quiltmc/quiltflower/kotlin/KotlinTests.java
@@ -29,57 +29,57 @@ public class KotlinTests extends SingleClassesTestBase {
   }
 
   private void registerKotlinTests() {
-    register(KOTLIN, "TestKt");
-    register(KOTLIN, "TestParams");
-    register(KOTLIN, "TestInfixFun");
-    register(KOTLIN, "TestFunVarargs");
-    register(KOTLIN, "TestSmartCasts");
-    register(KOTLIN, "TestTailrecFunctions");
-    register(KOTLIN, "TestTryCatchExpressions");
-    register(KOTLIN, "TestTryFinallyExpressions");
-    register(KOTLIN, "TestVars");
-    // TODO: handle lambdas
-    register(KOTLIN, "TestNonInlineLambda");
-    register(KOTLIN, "TestNothingReturns");
-    // TODO: Generics containing nullability (like List<String?>) lose the nullability
-    register(KOTLIN, "TestNullable");
-    register(KOTLIN, "TestExtensionFun");
-    register(KOTLIN, "TestClassDec");
+    // TODO: Annotation classes decompile entirely incorrectly
+    register(KOTLIN, "TestAnnotations");
     register(KOTLIN, "TestAnyType");
-    // TODO: gets condensed into ternaries
-    register(KOTLIN, "TestWhen");
-    register(KOTLIN, "TestWhenBoolean");
-    register(KOTLIN, "TestForRange");
-    register(KOTLIN, "TestIfRange");
+    register(KOTLIN, "TestBitwiseFunctions");
+    register(KOTLIN, "TestClassDec");
     register(KOTLIN, "TestComparison");
-    register(KOTLIN, "TestNullableOperator");
-    // TODO: x = x needs to not have the type attached
-    register(KOTLIN, "TestShadowParam");
-    register(KOTLIN, "TestWhenControlFlow");
-    register(KOTLIN, "TestLabeledJumps");
-    register(KOTLIN, "TestFuncRef");
-    // TODO: top-level constructs are wrongly put into a class and turned into instance methods
-    register(KOTLIN, "TestTopLevelKt");
+    register(KOTLIN, "TestCompileTimeErrors");
     // TODO: lists, sets, etc. should be considered to be their Kotlin type, not their Java type
     register(KOTLIN, "TestConvertedK2JOps");
     // TODO: Does not decompile to data class
     register(KOTLIN, "TestDataClass");
     // TODO: Does not decompile to destructuring declaration
     register(KOTLIN, "TestDestructors");
+    register(KOTLIN, "TestExtensionFun");
+    register(KOTLIN, "TestForRange");
+    register(KOTLIN, "TestFuncRef");
+    register(KOTLIN, "TestFunVarargs");
     // TODO: Type projections are not Kotlin equivalents
     register(KOTLIN, "TestGenerics");
+    register(KOTLIN, "TestIfRange");
+    register(KOTLIN, "TestInfixFun");
     // TODO: Nothing, function types
     register(KOTLIN, "TestKotlinTypes");
+    register(KOTLIN, "TestKt");
+    register(KOTLIN, "TestLabeledJumps");
+    // TODO: handle lambdas
+    register(KOTLIN, "TestNonInlineLambda");
+    register(KOTLIN, "TestNothingReturns");
+    // TODO: Generics containing nullability (like List<String?>) lose the nullability
+    register(KOTLIN, "TestNullable");
+    register(KOTLIN, "TestNullableOperator");
     // TODO: Does not decompile to object literal
     // FIXME: @JvmStatic function fails to decompile at all
     register(KOTLIN, "TestObject");
+    register(KOTLIN, "TestParams");
+    register(KOTLIN, "TestPoorNames");
     // TODO: sealed becomes "open abstract"
     register(KOTLIN, "TestSealedHierarchy");
-    // TODO: Annotation classes decompile entirely incorrectly
-    register(KOTLIN, "TestAnnotations");
-    register(KOTLIN, "TestBitwiseFunctions");
-    register(KOTLIN, "TestCompileTimeErrors");
-    register(KOTLIN, "TestPoorNames");
+    register(KOTLIN, "TestShadowParam");
+    register(KOTLIN, "TestSmartCasts");
     register(KOTLIN, "TestSynchronized");
+    register(KOTLIN, "TestTailrecFunctions");
+    // TODO: top-level constructs are wrongly put into a class and turned into instance methods
+    register(KOTLIN, "TestTopLevelKt");
+    register(KOTLIN, "TestTryCatchExpressions");
+    register(KOTLIN, "TestTryFinallyExpressions");
+    register(KOTLIN, "TestVars");
+    // TODO: gets condensed into ternaries
+    register(KOTLIN, "TestWhen");
+    register(KOTLIN, "TestWhenBoolean");
+    // TODO: x = x needs to not have the type attached
+    register(KOTLIN, "TestWhenControlFlow");
   }
 }
